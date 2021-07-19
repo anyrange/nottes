0172684c10584866b9c5303aaf044bc1da4c7e23
Alex Sehl <widesehl@gmail.com>
2021-07-19 14:00:31 +0600

    fix: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'require-await': false,
+    'require-await': 'off',
   },
 }
