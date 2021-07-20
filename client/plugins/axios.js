b05838d8ff5afef734e44e8198c499e19d94f1fd
Alex Sehl <widesehl@gmail.com>
2021-07-21 01:19:44 +0600

    fix: disable console.log on axios request

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -3,11 +3,6 @@ export default function ({ $axios, redirect, error: nuxtError }) {
     console.log('Making request to ' + config.url)
   })
   $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status)
-    if (code === 404) {
-      redirect('/404')
-    }
-    console.log(error.response.statusText)
     return Promise.reject(error)
   })
 }
