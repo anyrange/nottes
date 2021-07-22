805fb7dd1f18f2b3b3151756e273fe0280dcf5d9
Alex Sehl <widesehl@gmail.com>
2021-07-23 01:02:17 +0600

    feat: get recent pastes via event stream

--- client/components/TheSidebar.vue
+++ client/components/TheSidebar.vue
@@ -1,17 +1,22 @@
 <template>
   <aside class="flex flex-col gap-3">
     <h1 class="h-title">Recent Pastes</h1>
-    <div class="flex flex-col">
+    <div class="flex flex-col gap-3">
       <div v-for="paste in pastes" :key="paste.id" class="flex items-center justify-between">
-        <div class="flex flex-col">
-          <nuxt-link class="link" :to="'/' + paste.id">
-            {{ paste.name }}
+        <div class="flex flex-col w-full">
+          <nuxt-link class="link truncate w-11/12" :to="'/' + paste.id">
+            {{ paste.title }}
           </nuxt-link>
           <div class="text-xs">
             <span>by </span>
-            <nuxt-link class="hover:underline" :to="'/user/' + paste.author.username">
+            <template v-if="paste.author.username !== 'Guest'">
+              <nuxt-link class="hover:underline" :to="'/user/' + paste.author.username">
+                <i>{{ paste.author.username }}</i>
+              </nuxt-link>
+            </template>
+            <template v-else>
               <i>{{ paste.author.username }}</i>
-            </nuxt-link>
+            </template>
           </div>
         </div>
         <span v-tooltip:bottom-left="$timePassedFrom(paste.date)">
@@ -27,17 +32,22 @@ export default {
   name: 'TheSidebar',
   data() {
     return {
-      pastes: [
-        {
-          id: '1',
-          name: 'pastes',
-          author: {
-            username: 'string',
-            avatar: 'string',
-          },
-          date: '2021-07-20T16:13:58.109Z',
-        },
-      ],
+      pastes: [],
+    }
+  },
+  mounted() {
+    const evtStream = new EventSource(`${process.env.baseUrl}/api/pastes/recent`)
+    const pastes = this.pastes
+
+    evtStream.onmessage = (event) => {
+      const { data: paste } = JSON.parse(event.data)
+      pastes.push(paste)
+      if (pastes.length > 10) pastes.pop()
+    }
+
+    evtStream.onerror = (err) => {
+      console.error('EventSource failed:', err)
+      evtStream.close()
     }
   },
 }
