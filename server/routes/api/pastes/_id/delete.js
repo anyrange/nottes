abbce711c2d67d3223f4284853502ac326b1a9a7
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-07-22 14:51:21 +0600

    fix: change property

--- server/routes/api/pastes/_id/delete.js
+++ server/routes/api/pastes/_id/delete.js
@@ -24,7 +24,7 @@ module.exports = async function (fastify) {
       const paste = await fastify.db.Paste.findOne({ id: request.params.id }).lean()
       if (!paste) return reply.code(404).send({ message: 'Paste not found' })
 
-      if (request._id !== String(paste.user)) return reply.code(403).send({ message: 'Forbidden' })
+      if (request._id !== String(paste.author)) return reply.code(403).send({ message: 'Forbidden' })
 
       if (paste.password && !request.query.password) return reply.code(403).send({ message: 'Password required' })
       if (paste.password && !(await bcrypt.compare(request.query.password, paste.password)))
