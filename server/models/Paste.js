06e7d7d66746bc240d395a43506df5dcfa0b92f6
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-07-27 16:55:44 +0600

    feat: model timestamps

--- server/models/Paste.js
+++ server/models/Paste.js
@@ -3,17 +3,19 @@
 const { Schema, model } = require('mongoose')
 const { nanoid } = require('nanoid')
 
-const schema = new Schema({
-  _id: { type: String, default: () => nanoid(6) },
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
-  code: { type: String, default: 'text' },
-  visibility: { type: String, default: 'public' },
-  expiry: { type: Date },
-  password: { type: String },
-  views: { type: Number, default: 0 },
-  date: { type: Date, default: Date.now },
-})
+const schema = new Schema(
+  {
+    _id: { type: String, default: () => nanoid(6) },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    code: { type: String, default: 'text' },
+    visibility: { type: String, default: 'public' },
+    expiry: { type: Date },
+    password: { type: String },
+    views: { type: Number, default: 0 },
+  },
+  { timestamps: { createdAt: 'date', updatedAt: 'updated_at' } }
+)
 
 module.exports = model('Paste', schema)
--- server/models/User.js
+++ server/models/User.js
@@ -3,18 +3,15 @@
 const { Schema, model } = require('mongoose')
 const { nanoid } = require('nanoid')
 
-const schema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    default: () => `user_${nanoid(6)}`,
+const schema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, default: () => `user_${nanoid(6)}` },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    avatar: { type: String },
+    platform: { type: String, default: 'Direct', required: true },
   },
-  email: { type: String, required: true, unique: true },
-  password: { type: String },
-  avatar: { type: String },
-  platform: { type: String, default: 'Direct', required: true },
-  registered: { type: Date, default: Date.now },
-})
+  { timestamps: { createdAt: 'registered' } }
+)
 
 module.exports = model('User', schema)
