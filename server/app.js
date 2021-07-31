fae3fb4b3eacd7ca5cf7a62c95d964809317e297
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-07-31 14:14:53 +0600

    refactor: move session logic out of app file

--- server/app.js
+++ server/app.js
@@ -24,15 +24,6 @@ module.exports = async function (fastify) {
     console.log('\x1B[36m%s\x1B[0m', `i`, `Docs: ${process.env.BASE_URL}/docs`)
   }
 
-  const domain = new URL(process.env.BASE_URL).hostname
-  const MONTH = 60 * 60 * 24 * 30
-
-  fastify.register(require('fastify-secure-session'), {
-    cookieName: 'session',
-    key: Buffer.from(process.env.COOKIE_SECRET, 'hex'),
-    cookie: { sameSite: 'strict', domain, path: '/', secure: true, maxAge: MONTH },
-  })
-
   fastify.register(require('fastify-websocket'))
 
   fastify.register(AutoLoad, { dir: path.join(__dirname, 'plugins') })
--- server/plugins/auth/session.js
+++ server/plugins/auth/session.js
@@ -3,6 +3,22 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify) {
+  const domain = new URL(process.env.BASE_URL).hostname
+  const MONTH = 60 * 60 * 24 * 30
+
+  fastify.register(require('fastify-secure-session'), {
+    cookieName: 'session',
+    key: Buffer.from(process.env.COOKIE_SECRET, 'hex'),
+    cookie: {
+      sameSite: 'strict',
+      domain,
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      maxAge: MONTH,
+    },
+  })
+
   fastify.decorate('auth', async function (request, reply) {
     if (!request.session.get('_id')) return reply.code(401).send({ message: 'Unauthorized' })
   })
