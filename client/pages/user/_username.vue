c9fde4df6297acb244da3d251eeab85d844c98f0
Alex Sehl <widesehl@gmail.com>
2021-08-04 19:52:25 +0600

    feat: update paste page

---
+++ client/pages/user/_username.vue
@@ -0,0 +1,63 @@
+<template>
+  <main class="h-main">
+    <div class="flex flex-row gap-3 items-center">
+      <img
+        v-if="user.avatar"
+        class="rounded-full w-22 h-22 flex-none select-none"
+        :src="user.avatar"
+        :alt="user.username"
+      />
+      <div class="flex flex-col">
+        <h1 class="h-title">{{ user.username }}</h1>
+        <h2>Joined: {{ $formattedDate(user.registered) }}</h2>
+      </div>
+    </div>
+    <div>
+      <h3 class="text-lg font-medium">Some of your statistics:</h3>
+      <ul>
+        <li>Total Pastes: {{ user.stats.total }}</li>
+        <li>Total views of all your pastes: {{ user.stats.views }}</li>
+        <li>Public pastes: {{ user.stats.public }}</li>
+        <li>Unlisted pastes: {{ user.stats.unlisted }}</li>
+        <li>Private pastes: {{ user.stats.private }}</li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+import { getUserPage } from '@/api'
+
+export default {
+  async asyncData({ route, params, error }) {
+    try {
+      const { user, pastes } = await getUserPage(params.username)
+      return { user, pastes }
+    } catch (err) {
+      error(err.response.data)
+    }
+  },
+  data() {
+    return {
+      user: {},
+      pastes: [],
+    }
+  },
+  head() {
+    const title = this.user.username || 'nottes'
+    const description = 'nottes-description'
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'og:title', property: 'og:title', content: title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description,
+        },
+      ],
+    }
+  },
+}
+</script>
--- client/plugins/dates.js
+++ client/plugins/dates.js
@@ -1,9 +1,15 @@
-import { formatDistanceToNowStrict } from 'date-fns'
+import { formatDistanceToNowStrict, format } from 'date-fns'
 
 export default ({ app }, inject) => {
-  inject('timePassedFrom', function (date) {
+  inject('timePassedFrom', (date) => {
     return formatDistanceToNowStrict(Date.parse(date), {
       addSuffix: true,
     })
+  })
+  inject('formattedDate', (date) => {
+    return format(Date.parse(date), 'Mo MMMM yyyy')
+  })
+  inject('defaultDateTime', (date) => {
+    return format(Date.parse(date), 'yyyy/MM/dd hh:mm:ss')
   })
 }
