86e09e84fd00e1038d9fcf62c20b77398dab9b3e
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-08-05 12:46:05 +0600

    feat: paste download api

---
+++ server/routes/download/_id/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const bcrypt = require('bcrypt')
+
+module.exports = async function (fastify) {
+  fastify.get(
+    '',
+    {
+      schema: {
+        params: {
+          type: 'object',
+          properties: { id: { type: 'string' } },
+        },
+        querystring: {
+          type: 'object',
+          properties: { password: { type: 'string' } },
+        },
+        tags: ['layout (Base URL: /)'],
+      },
+    },
+    async (request, reply) => {
+      const paste = await fastify.db.Paste.findById(request.params.id, 'content visibility author').lean()
+
+      if (!paste) return reply.code(404).send({ message: 'Paste not found' })
+
+      if (paste.visibility === 'private' && request.session.get('_id') !== String(paste.author)) {
+        return reply.code(403).send({ message: 'Private paste' })
+      }
+
+      if (paste.password && !request.query.password) return reply.code(403).send({ message: 'Password required' })
+      if (paste.password && !(await bcrypt.compare(request.query.password, paste.password)))
+        return reply.code(403).send({ message: 'Wrong password' })
+
+      reply.headers({
+        'Content-Type': 'multipart/form-data',
+        'Content-Disposition': `filename="${request.params.id}.txt"`,
+      })
+      reply.send(fastify.decrypt(paste.content))
+    }
+  )
+}
