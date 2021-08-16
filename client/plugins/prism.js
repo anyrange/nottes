b88d05d7ac125b1e0de4fd0216633f4172716b8d
Alex Sehl <widesehl@gmail.com>
2021-08-16 23:55:02 +0600

    feat: code highlight

---
+++ client/plugins/prism.js
@@ -0,0 +1,43 @@
+import 'clipboard'
+
+import Prism from 'prismjs'
+
+// import 'prismjs/themes/prism.css'
+// import 'prismjs/themes/prism-tomorrow.css'
+import 'prism-theme-vars/base.css'
+
+// const md = require('markdown-it')();
+// const prism = require('markdown-it-prism');
+
+// import MarkdownIt from 'markdown-it'
+// import prism from 'markdown-it-prism'
+
+import 'prismjs/components/prism-markup'
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-c'
+import 'prismjs/components/prism-csharp'
+import 'prismjs/components/prism-cpp'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-javascript'
+
+import 'prismjs/plugins/toolbar/prism-toolbar'
+import 'prismjs/plugins/toolbar/prism-toolbar.css'
+
+import 'prismjs/plugins/autolinker/prism-autolinker'
+import 'prismjs/plugins/autolinker/prism-autolinker.css'
+
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+import 'prismjs/plugins/line-highlight/prism-line-highlight'
+import 'prismjs/plugins/line-highlight/prism-line-highlight.css'
+
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
+import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords'
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
+
+// const md = new MarkdownIt()
+
+// md.use(prism)
+
+export default Prism
