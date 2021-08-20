f521f64c3cbd14b92f14e7729335dbcb1e45c8cb
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-08-20 12:13:52 +0600

    feat: clear diff

--- client/utils/diff.js
+++ client/utils/diff.js
@@ -1,5 +1,16 @@
 import { createPatch } from 'diff'
 
 export const getCodeDiff = ({ title, from, to }) => {
-  return createPatch(title, from, to)
+  const diff = createPatch(title, from, to)
+  const clearDiff = diff
+    .replace(/.*/, '')
+    .substr(1)
+    .replace(/.*/, '')
+    .substr(1)
+    .replace(/.*/, '')
+    .substr(1)
+    .replace(/.*/, '')
+    .substr(1)
+
+  return clearDiff || 'No changes'
 }
