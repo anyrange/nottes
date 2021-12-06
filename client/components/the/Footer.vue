39114abfed1c82da4e787c4ca1e82bd95b9a266a
Alex Sehl <widesehl@gmail.com>
2021-12-06 23:32:16 +0600

    feat: github link

--- client/components/the/Footer.vue
+++ client/components/the/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="border-t default-border">
     <div class="h-container h-16 flex gap-3 justify-between items-center">
-      <span>nottes@{{ currentYear }}</span>
+      <p>
+        <span>nottes@{{ currentYear }}</span>
+        <a class="underline" href="https://github.com/anyrange/nottes"> github </a>
+      </p>
       <theme-toggler />
     </div>
   </footer>
